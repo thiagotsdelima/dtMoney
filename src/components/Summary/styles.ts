a82f5a0bf32dbe5370bbfb85060fcab8e74f1479
thiago <thiagotsdelima@gmail.com>
2024-04-09 10:35:25 -0300

    update general

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const SummaryContainer = styled.section`
 width: 100%;
@@ -9,4 +9,28 @@ grid-template-columns: repeat(3, 1fr);
 gap: 2rem;
 margin-top: -5rem;
 
+`;
+
+interface SummaryCardProps {
+  variant?: 'green';
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+background: ${props => props.theme['gray-600']};
+border-radius: 6px;
+padding: 2rem;
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${props => props.theme['gray-300']};
+}
+strong {
+  display: block;
+  margin-top: 1rem;
+  font-size: 2rem;
+}
+${props => props.variant === 'green' && css`
+background: ${props => props.theme['green-700']};
+`}
 `;
