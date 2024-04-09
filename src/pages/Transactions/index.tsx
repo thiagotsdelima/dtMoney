dd39928b322ccb49cb09e5842b29cfb448b90144
thiago <thiagotsdelima@gmail.com>
2024-04-09 19:35:21 -0300

    update

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
@@ -7,6 +8,32 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Website development</td>
+            <td>
+              <PriceHighlight variant="income">
+                R$ 12.000,00
+              </PriceHighlight>
+           </td>
+            <td>Sales</td>
+            <td>13/02/2024</td>
+          </tr>
+          <tr>
+            <td width="50%">Hamburger</td>
+            <td>
+              <PriceHighlight variant="outcome">
+              - R$ 59,00
+              </PriceHighlight>
+            </td>
+            <td>Food</td>
+            <td>10/02/2024</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
